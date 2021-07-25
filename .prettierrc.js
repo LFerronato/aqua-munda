989aef654c9d490e9362b699778a691bafbf834c
Lucas Ferronato <lucas.fe.pelle@gmail.com>
2021-07-25 18:12:44 -0300

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// Change your rules accordingly to your coding style preferences.
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  endOfLine:"auto",
+  semi: false,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  useTabs: false,
+}
